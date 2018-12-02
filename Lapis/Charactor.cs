4e87979509241e1af086bbe0ab3e70d6ad87c60a
turtle-insect <turtle.insect@gmail.com>
2018-12-03 00:36:40 +0900

    first commit

---
+++ Lapis/Charactor.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace Lapis
+{
+	class Charactor : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public Charactor(uint address)
+		{
+			mAddress = address;
+		}
+
+		public String Name
+		{
+			get { return SaveData.Instance().ReadText(mAddress, 21); }
+			set
+			{
+				SaveData.Instance().WriteText(mAddress, 21, value);
+				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Name)));
+			}
+		}
+
+		public uint Color
+		{
+			get { return SaveData.Instance().ReadNumber(mAddress + 32, 1); }
+			set { SaveData.Instance().WriteNumber(mAddress + 32, 1, value); }
+		}
+
+		public uint Hair
+		{
+			get { return SaveData.Instance().ReadNumber(mAddress + 36, 1); }
+			set { SaveData.Instance().WriteNumber(mAddress + 36, 1, value); }
+		}
+
+		public uint Voice
+		{
+			get { return SaveData.Instance().ReadNumber(mAddress + 40, 1); }
+			set { SaveData.Instance().WriteNumber(mAddress + 40, 1, value); }
+		}
+
+		public uint Attack
+		{
+			get { return SaveData.Instance().ReadNumber(mAddress + 96, 4); }
+			set { SaveData.Instance().WriteNumber(mAddress + 96, 4, value); }
+		}
+
+		public uint HP
+		{
+			get { return SaveData.Instance().ReadNumber(mAddress + 100, 4); }
+			set { SaveData.Instance().WriteNumber(mAddress + 100, 4, value); }
+		}
+
+
+		private readonly uint mAddress;
+	}
+}
---
+++ Lapis/DataContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace Lapis
+{
+	class DataContext
+	{
+		public ObservableCollection<Charactor> Party { get; set; } = new ObservableCollection<Charactor>();
+
+		public DataContext()
+		{
+			for(uint i = 0; i < 32; i++)
+			{
+				Party.Add(new Charactor(0x10 + i * 200));
+			}
+		}
+
+		public uint Money
+		{
+			get { return SaveData.Instance().ReadNumber(0x56E0, 4); }
+			set { Util.WriteNumber(0x56E0, 4, value, 9999999, 0); }
+		}
+	}
+}
